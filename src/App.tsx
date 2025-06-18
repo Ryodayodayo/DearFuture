2e51a65c395c453d3cb87331b535dfddd5c87364
Ryodayodayo <mitosara10@gmail.com>
2025-06-18 20:26:54 +0900

    ヘッダー作った

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <AuthProvider>
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path = "/" element = {<Home />}/>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import styles from "./Header.module.css"
+
+
 export const Header = () => {
-    return (
-        <div>
-            HelloWorld
-        </div>
-    );
-} 
+  return (
+    <div className={styles.header}>
+      <h1 className="text-xl font-bold">DearFuture</h1>
+    </div>
+  );
+};
