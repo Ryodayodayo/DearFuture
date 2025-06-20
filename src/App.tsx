f179fedd5cd58c74818c8fa9dea5d54033d1310a
Ryodayodayo <mitosara10@gmail.com>
2025-06-21 02:43:39 +0900

    refactor: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
-import { Login } from "./pages/Login"
-import { Signup } from "./pages/Signup"
-import { DiaryPage } from "./pages/DiaryPage"
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { DiaryPage } from "./pages/DiaryPage";
 import { AuthProvider } from './contexts/AuthContext';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PublicRoute } from './routes/PublicRoute';
 import { PrivateRoute } from './routes/PrivateRoute';
 
